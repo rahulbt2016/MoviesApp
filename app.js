7edc2f3675490575f5570087248722a070f1a342
Rahul <rahulbt2016@gmail.com>
2022-11-29 05:41:42 -0500

    Added 2 routes

--- app.js
+++ app.js
@@ -22,22 +22,63 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 
-mongoose.connect(database.url);
+mongoose.connect(database.url)
+.then(() => {
+	app.listen(port);
+	console.log("App listening on port : " + port);
+})
+.catch((err) => {console.log(err)})
 
 var Movie = require('./models/movie');
+const { title } = require('process');
 
  
-//get all books data from db
+//get all movies data from db
 app.get('/api/movies', function(req, res) {
-	// use mongoose to get all todos in the database
-	Movie.find(function(err, movies) {
-		// if there is an error retrieving, send the error otherwise send data
-		if (err)
-			res.send(err)
-		res.json(movies); // return all books in JSON format
+
+	Movie.find().count((err, count) => {
+		
+		let perPage = count;
+		let page = 0;
+		let filter = {};
+
+		if(req.query.page && req.query.page > 0)
+			page = req.query.page;
+		
+		if(req.query.perPage && req.query.perPage > 0)
+			perPage = req.query.perPage;
+		
+		let skip = page * perPage;
+
+		if(req.query.title) {
+			let title = req.query.title;
+			let regex = new RegExp(title, "i");
+
+		filter = {title: regex};
+		}
+		
+		Movie.find(filter, {},  {skip: skip, limit: perPage}, (err, movies) => {
+			if (err)
+				res.send(err)
+ 
+			res.json(movies);
+		});
 	});
+
+	
 });
 
-app.listen(port);
-console.log("App listening on port : " + port);
+// get a movie from db by its id
+app.get('/api/movies/:id', function(req, res) {
+	let id = req.params.id;
+	Movie.findById(id, function(err, book) {
+		if (err)
+			res.send(err)
+ 
+		res.json(book);
+	});
+ 
+});
+
+
 
