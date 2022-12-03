279ef15ce8754903face90b54d0af200d65362e9
Rahul <rahulbt2016@gmail.com>
2022-12-03 12:01:38 -0500

    Added frontend for Insert Movie

--- public/js/main.js
+++ public/js/main.js
@@ -79,4 +79,44 @@ $( document ).ready(function() {
         
     });
 
+    var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
+        removeItemButton: true,
+        maxItemCount:10,
+        searchResultLimit:10,
+        renderChoiceLimit:10
+      }); 
+ 
+      var now = new Date();
+  
+      var day = ("0" + now.getDate()).slice(-2);
+      var month = ("0" + (now.getMonth() + 1)).slice(-2);
+  
+      var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+  
+  
+     $('#date').val(today); 
+     $('#date1').val(today); 
+     $('#date2').val(today); 
+     $('#date3').val(today); 
+
+     $("#add-movie-form-btn").click(function(){
+        
+        if(confirm("Add movie?")) {
+
+            $.ajax({
+                url: '/api/movies/',
+                type: 'POST',
+                data: {
+                    title : $("[name='title']").val(),
+                    countries : $("[name='countries']").val().toString()
+                },
+                success: function(result) {
+                    alert("Movie added successfully!");
+                    window.location.replace("/");
+                }
+            });
+        }
+
+     });
+
 });
